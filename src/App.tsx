7d64b5c877142859f9b3d2225f331c40d14960da
Karthik <karao@eg.dk>
2025-01-17 16:04:17 +0530

    feat: add necessary routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router";
 
 function App() {
-
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route index element={<h1>Home</h1>} />
+				<Route path="/article/:id" element={<h1>Articles</h1>} />
+				<Route path="*" element={<h1>Error</h1>} />
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
-export default App
+export default App;
