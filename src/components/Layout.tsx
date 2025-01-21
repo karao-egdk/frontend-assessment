2cb25bfc3ae2f7c96eb6f00829812b4599528194
Karthik <karao@eg.dk>
2025-01-21 14:01:19 +0530

    refactor: fix the semantic element in Layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Layout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="container mx-auto w-full h-full py-4 sm:px-0 px-2">
+		<section className="container mx-auto w-full h-full py-4 sm:px-0 px-2">
 			{children}
-		</div>
+		</section>
 	);
 }
 
